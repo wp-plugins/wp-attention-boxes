b12f887e6e764b91b3f4759cd900fd89cebe9df7
pythoPhpSteve <pythoPhpSteve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-15 14:41:51 +0000

    Removed buttons from toolbar - the metabox under editor will be used to add snippets, and tagging to tag: 0.5.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-attention-boxes/tags/0.5.0@475918 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,33 +2,28 @@
 Contributors: pythoPhpSteve
 Tags: attention, styled divs, emphasis, snippets, code snippets
 Requires at least: 2.7
-Tested up to: 3.21
-Stable tag: 0.3
+Tested up to: 3.3
+Stable tag: 0.5.0
 
 A specialized code snippet tool that provides one click access, when editing posts, to 4 different CSS-styled DIVs for custom purposes.
 
-
 == Description ==
-
-
 
 Sometimes you just want to call out attention to a particular important message, or quote, or a collection of links, in a
 bright yellow box, or similar, in a Blog post.  And you might do this a lot in your blog entries. 
 
 This plugin keeps a snippet library of these stylized DIV's, and makes them immediately accessible in your edit post page by clicking a button on your html-mode toolbar.
 
-
-
 The contact page for this plugin is: <a href="http://stevebailey.biz/blog/wp-attention-boxes" target="_blank">Wp Attention Boxes</a>
-
-Currently the screen shots don't seem to be showing for this plugin, on the page you're looking at, so you can see the screenshots at the site above
-
 
 == Installation ==
 
-Instructions for installing the WP Attention Boxes Plugin.
-  
-  [ Note: If you're upgrading, please first de-activate this plugin. ]
+* You can use Zip-uploader feature if your version of Wordpress has it
+1. Download the Plugin as Zip file, but DON'T extract the Zip File
+2. In your Admin, Go to Plugins/ Add New / and then click Upload from the choices .. browse for the Zip that you downloaded
+3. Click Activate
+
+* OR do it the manual way:
 
 1. Download and extract the Plugin zip file.
 2. Upload the folder containing the Plugin files to your WordPress Plugins folder (usually ../wp-content/plugins/ folder).
@@ -37,12 +32,16 @@ Instructions for installing the WP Attention Boxes Plugin.
 5. There will be four default DIV's already there which may serve your purpose, however of course change them as you need.
 
 == Frequently Asked Questions ==
-= The preview box in the setting page doesn't seem to reflect the change I just made =
+= The Newly designed Settings Page doesn't look like the one in the screenshot =
+Please click the browser refresh button (f5 if you're on Windows), and if that doesn't do it, clear your Browser Cache, so that the new CSS styles will overwrite the ones that are still cached by your browser.
+
+= The preview box in the new 0.4 setting page doesn't seem to reflect the change I just made =
 Yes, this is how the Javascript onchange event works.. after you change a CSS element, you'll need to tab out of the field - then the preview box will update itself.
 
 = I notice, with this version 0.4, that I now need to specify "px" in the border width =
-Yes, this way the more knowledgeable users in CSS can use other measurement units such as "em".   If you do, by chance, enter in just a number by itself, I added a validation routine that automatically tacks on "px".  But you can use px, em, or whatever.
-Bottom Line: You shouldn't need to do anything when  you upgrade, install routine should automatically upgrade your current border settings to "1px" (if it was just 1, you get the drift)
+Yes, this way the more knowledgeable users in CSS can use other measurement units such as "em".  
+ If you do, by chance, enter in just a number by itself, I added a validation routine that automatically tacks on "px".  But you can use px, em, or whatever.
+Bottom Line: You won't need to do anything when you upgrade to 0.4 - the install routine should automatically upgrade your current border settings to "1px" (if it was just 1, you get the drift)
     
 = What if I use the Visual Editor when creating pages/posts ? =
 This plugin only works when in the Html editor - i.e. this is basically for users who prefer the HTML tab of the editor, or at least switch to it for tools like this one.
@@ -52,28 +51,42 @@ My advice, to be able to use tools in the Html editor (whether it's this plugin 
 
 = Does the plugin change the content in the WordPress database? =
 
- Yes, it adds a single record to the wp_options table. This stores all of the custom DIV's.
-No other changes to the database. 
+Yes, it adds 2 record to the wp_options table. One of them stores all of the custom DIV's.
+ The other simple stores the version number of this plugin.
+
 
 = Do you plan on adding the ability to store snippets for other types of html elements? =
 Yes, the next major version will provide the ability to store different styles of your own image-based (or non-imaged based) bullet lists
 
-= Why only 4 =
-Good question, a future major version will provide 10
-
-
 
 == Screenshots ==
 
-1. Html editor bar after new buttons are added (if the buttons consume too much space, you can disable one or more, or name them as short as you want)
+1. This "display meta-box" underneath the Editor is where you choose which snippet to add to your post, via simply clicking on the DIV you want.
 2. This is the options page, used to define the styling of each of the 4 DIV's. 
-   NEW on this page: The floating preview div, and the design of this page designs
 3. Actual stylized box examples (just as an example of the variation of styles, just the border type alone could be: solid, dotted, dashed, double, groove, ridge, inset, outset )
-4. Simply a convenient "display meta-box" that shows up underneath where you do your actual post or page editing, so you don't have to return to the settings to remember what they look like.
+
 
 == Changelog ==
+= 0.5.0 =
+  Removed the buttons from the Post/Page edit toolbar .. the DIV snippets are now added to your post via simply clicking on your chosen DIV directly from the Meta-Box below the Editor
+
+= 0.4.2 =
+  Internal variable changes to ensure that the javascript handler names don't conflict with other plugins, and some other minor changes
+
+= 0.4.1 =
+Fixed a bug in the CSS code that is generated when rounded corners are enabled for any of the DIV's.. Specifically, removed an extra quote in the style that might prevent the rounded corner from working.
+
+= 0.4 =
+* Added support for Rounded Corners
+* A Preview Box so that you know what your DIVs look like before leaving the Settings page
 
 = 0.3 =
 * Added a convenient visual reference section that shows you what your DIV's look like w/o having to save your post and switch to this plugin's Settings page
+
 = 0.2 =
 * Bugfix .. there was an extra quote in the generated snippet and added a Tip to this plugin's f.a.q.
+
+== Upgrade Notice ==
+= 0.5.0 =
+This is not an optional upgrade if you have upgraded to Wordpress version 3.3. On V 3.3, the toolbar buttons are missing, thus breaking this plugin's functionality.
+Regardless of the WP version you're using, I'd recommend the upgrade - even below V 3.3, your HTML toolbar will be uncluttered by the buttons and this makes it easier for a future version to have many more DIV snippets.
--- wp-attention-boxes.php
+++ wp-attention-boxes.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Attention Boxes
 Plugin URI: http://stevebailey.biz/blog/wp-attention-boxes
 Description: Instantly add four of your most commonly used CSS-styled DIV's for different purposes, such as bringing attention to an important update, or just bringing visual focus to a quote..
-Version: 0.3
+Version: 0.5.0
 Author: Steve Bailey
 Author URI: http://stevebailey.biz/
 License: GPL
@@ -26,170 +26,180 @@ Copyright 2010 Steve Bailey (email steveswdev@gmail.com)
 
 */
 
-include "attn_boxes_admin_menu.php";
+include_once "includes/wp-attention-includes.inc";
 
-new Attention_Box;  # looks weird, but don't need to assign the instantiated object to a variable. 
-                    # The functions are merely being collected into classes in order to remove the possibility of 
-                    # duplicate function call conflicts with other plugins
+include_once "attn_boxes_admin_menu.php";
 
-class Attention_Box {
+add_action( 'admin_init', 'wp_attn_boxes_add_div_carousel');
+//add_action( 'admin_init', 'carousel');
+add_filter('admin_footer', 'wp_attnbox_add_js_insert_handlers');
+add_action( 'wp_print_styles', 'wp_attn_box_enqueue_my_styles' );
+register_activation_hook(__FILE__, 'attnbox_register_hook');
 
-    function __construct() {
-        new Attention_Box_Options;    // starts necessary options Initialization hooks located in Options page's class, via its own __construct
-        add_action( 'wp_print_styles', array( &$this,'enqueue_my_styles') );
-        register_activation_hook(__FILE__, array( &$this,'attnbox_add_defaults' ));
-        add_filter('admin_footer', 'wp_attnbox_add_quicktag');
-    }
-   
-   
-   
-    function attnbox_add_defaults() {
-        $ar = get_option('attnbox_options');
-        if (!is_array($ar)) {
-            $temp_arr = array("box_name_1" => "Yellow Update", "box_name_2" => "DownloadDiv", 
-                              "box_name_3" => "Quote Div", "box_name_4" => "Summary Box",
-                              "enable_div1" => "1", "enable_div2" => "1", "enable_div3" => "1", "enable_div4" => "1",
-                              "color1" => "black", "backcolor1" => "yellow", "bstyle1" => "solid", "bwidth1" => "2","bcolor1" => "red",
-                              "color2" => "black", "backcolor2" => "#FFFACD", "bstyle2" => "dashed", "bwidth2" => "2","bcolor2" => "black",
-                              "color3" => "black", "backcolor3" => "#FFFFFF", "bstyle3" => "ridge", "bwidth3" => "4","bcolor3" => "black",
-                              "color4" => "black", "backcolor4" => "#EEEEEE", "bstyle4" => "solid", "bwidth4" => "1","bcolor4" => "#BBBBBB"
-                              );
-            update_option('attnbox_options', $temp_arr);
-        }
+
+
+function wp_attn_box_enqueue_metabox_styles() {
+	$myStyleUrl = WP_PLUGIN_URL . '/wp-attention-boxes/css/attnbox_postmetabox_styles.css';
+	$myStyleFile = WP_PLUGIN_DIR . '/wp-attention-boxes/css/attnbox_postmetabox_styles.css';
+	if ( file_exists($myStyleFile) ) {
+		wp_register_style('my_wpattn_box_metabox_StyleSheets', $myStyleUrl);
+		wp_enqueue_style( 'my_wpattn_box_metabox_StyleSheets');
 	}
+}
 
-   // The Wordpress-preferred method of adding CSS needed by plugin.
-   // Basically making this plugin's styles.css available to posts   
-   function enqueue_my_styles() {
-      $myStyleUrl = WP_PLUGIN_URL . '/wp-attention-boxes/css/styles.css';
-        $myStyleFile = WP_PLUGIN_DIR . '/wp-attention-boxes/css/styles.css';
-        if ( file_exists($myStyleFile) ) {
-            wp_register_style('my_wpattn_box_StyleSheets', $myStyleUrl);
-            wp_enqueue_style( 'my_wpattn_box_StyleSheets');
-        }
-   }
+
+function wp_box_div_carousel ( $post ) {
+
+  // print the configured boxes ________________________________
+	echo '<div  class="outer_div_post_page">';
+	$options = get_option('attnbox_options'); 
+	foreach (range(1,4) as $indx) {
+		$style = 'style="color:' . $options['color' . $indx] . ';';
+		$style .= 'background-color:' . $options['backcolor' . $indx] . ';';
+		$style .= 'border:' . $options['bwidth' . $indx];
+		$style .= ' ' . $options['bstyle' . $indx];
+		$style .= ' ' . $options['bcolor' . $indx] . ';';
+		if (isset($options['enable_rounded' . $indx]) && ($options['enable_rounded'.$indx] == "1")) {
+			$style .= ' -webkit-border-radius: ' . $options['roundsize' . $indx] . ';';
+			$style .= ' -moz-border-radius: ' . $options['roundsize' . $indx] . ';';
+			$style .= ' border-radius: ' . $options['roundsize' . $indx] . ';';
+		}
+		$style .= ' text-align: ' . $options['align' . $indx] . ';"';
+		echo '<div class="meta_box_size" onclick="wp_attnbox_handler_' . $indx . '()"' . $style .  '>' . $options['box_name_' . $indx] . '</div>';
+	}
+  
+	echo '</div>'; 
+}
+    
+
+function wp_attn_boxes_add_div_carousel() {
+	wp_attn_box_enqueue_metabox_styles();
+	add_meta_box( 
+		'attnbox_sectionid',
+		__( 'Your Attention Boxes', 'attnbox_textdomain' ),
+		'wp_box_div_carousel',
+		'post' 
+	);
+    
+	add_meta_box( 
+		'attnbox_sectionid',
+		__( 'Your Attention Boxes', 'attnbox_textdomain' ),
+		'wp_box_div_carousel',
+		'page' 
+	);
+}
+
+wp_attn_box_run_upgrade_procedure();
+   
+function attnbox_register_hook() {
+	global $wp_version; 
+	if (version_compare($wp_version, "2.7", "<")) { 
+		deactivate_plugins(basename(__FILE__)); // Deactivate our plugin
+		wp_die("This plugin requires WordPress version 2.7 or higher.");
+	}
+}
+
+function wp_attn_box_run_upgrade_procedure() {
+	global $wp_attention_box_version;
+
+	$pre_four_version = array("0.1", "0.2", "0.3", NULL);
+	$options = get_option('attnbox_options'); 
+	$attn_box_version = get_option('wp_attention_box_version');
+ 
+	if (is_array($options)) {
+		if ( (empty($attn_box_version)) || ($attn_box_version != $wp_attention_box_version )) {
+			if ((in_array($attn_box_version, $pre_four_version))) {
+	    		// first, add default rounded corner widths if they're not already set, but don't actually enable rounded
+				// also, add default alignment of center
+				foreach ( range(1,4) as $indx ) {
+					if (empty( $options['roundsize'.$indx] )) {
+						$options['roundsize'.$indx] = "10px";
+					}
+					if (empty( $options['align'.$indx] )) {
+						$options['align'.$indx] = "center";
+					}
+				}
+			// then, make sure user is not using a version of this plugin that using plain integers for border widths
+			// if so, add a default "px"
+				foreach ( range(1,4) as $indx ) {
+					if (is_numeric( $options['bwidth'.$indx] )) {
+						$options['bwidth'.$indx] .= "px";
+					}
+				}
+				update_option('attnbox_options', $options);   
+			}
+			update_option('wp_attention_box_version', $wp_attention_box_version);
+		}
+
+
+	} // if options array exists
+	else {
+		update_option('wp_attention_box_version', $wp_attention_box_version);
+	}
+}
+   
    
 
-} // end of class
+// The Wordpress-preferred method of adding CSS needed by plugin.
+// Basically making this plugin's styles.css available to posts   
+function wp_attn_box_enqueue_my_styles() {
+  $myStyleUrl = WP_PLUGIN_URL . '/wp-attention-boxes/css/styles.css';
+    $myStyleFile = WP_PLUGIN_DIR . '/wp-attention-boxes/css/styles.css';
+    if ( file_exists($myStyleFile) ) {
+        wp_register_style('my_wpattn_box_StyleSheets', $myStyleUrl);
+        wp_enqueue_style( 'my_wpattn_box_StyleSheets');
+    }
+}
+   
 
-// This hairy looking function which opens and closes php tags over and over, is what generates the necessary javascript that adds the buttons
-// to the edit post page.
+
+
 // ---------------------------------------------------------------------------------------------------------------------------------------------
-function wp_attnbox_add_quicktag() {
+function wp_attnbox_add_js_insert_handlers() {
 	if (strpos($_SERVER['REQUEST_URI'], 'post.php') || strpos($_SERVER['REQUEST_URI'], 'post-new.php') || strpos($_SERVER['REQUEST_URI'], 'page-new.php') || strpos($_SERVER['REQUEST_URI'], 'page.php')) {
-?>
-<script type="text/javascript">//<![CDATA[
-	var toolbar = document.getElementById("ed_toolbar");
-<?php
-    $options = get_option('attnbox_options'); 
-    if (isset($options['enable_div1']))
-    	edit_wpattnbox_insert_button($options['box_name_1'], "wp_attnbox1_handler", "Attn Box 1");
-    if (isset($options['enable_div2']))
-    	edit_wpattnbox_insert_button($options['box_name_2'], "wp_attnbox2_handler", "Attn Box 2");
-    if (isset($options['enable_div3']))
-    	edit_wpattnbox_insert_button($options['box_name_3'], "wp_attnbox3_handler", "Attn Box 3");
-    if (isset($options['enable_div4']))
-    	edit_wpattnbox_insert_button($options['box_name_4'], "wp_attnbox4_handler", "Attn Box 4");    	    	    	
+		$options = get_option('attnbox_options'); 
+		foreach (range(1,4) as $indx) {
+		?>
+            <script type="text/javascript">//<![CDATA[
+			function wp_attnbox_handler_<?php echo $indx; ?>() {
+		    
+				textcolor = "<?php echo $options['color' . $indx]; ?>";
+				backcolor = "<?php echo $options['backcolor'  . $indx]; ?>";
+				border_width = "<?php echo $options['bwidth'  . $indx]; ?>";
+				border_color = "<?php echo $options['bcolor'  . $indx]; ?>";
+				border_style = "<?php echo $options['bstyle'  . $indx]; ?>";
+				textalign = "<?php echo $options['align' . $indx]; ?>";
+				
+				roundenabled = <?php if (isset($options['enable_rounded'  . $indx]))
+				                         echo $options['enable_rounded'  . $indx];
+				                       else 
+				                         echo "0";
+				                     ?>;
+				borderradius = "<?php echo $options['roundsize'  . $indx]; ?>";
+				
+				styled_div = '\n<div class="custom_attn_box" style="border: ';
+				styled_div += border_width;
+				styled_div += ' ' + border_style;
+				styled_div += ' ' + border_color + ';';
+				styled_div += ' color: ' + textcolor + ';';
+				styled_div += ' background-color: ' + backcolor + ';';
+				
+				if (roundenabled == 1) {
+				  styled_div += ' -webkit-border-radius: ' + borderradius + ';';
+				  styled_div += ' -moz-border-radius: ' + borderradius + ';';
+				  styled_div += ' border-radius: ' + borderradius + ';';
+				}
+				
+				styled_div += ' text-align: ' + textalign + ';"';
+				styled_div += ">your text</div>\n";
+				myValue = styled_div;
+				edInsertContent(edCanvas, myValue);
+			}
+			//]]></script>
+<?php	}
 	
-?>
-	var state_my_button = true;
-
-
-function wp_attnbox1_handler() {
-    
-    textcolor = "<?php echo $options['color1']; ?>";
-    backcolor = "<?php echo $options['backcolor1']; ?>";
-    border_width = "<?php echo  $options['bwidth1']; ?>";
-    border_color = "<?php echo $options['bcolor1']; ?>";
-    border_style = "<?php echo $options['bstyle1']; ?>";
-    
-    styled_div = '\n<div class="custom_attn_box" style="border: ';
-    styled_div += border_width;
-    styled_div += "px";
-    styled_div += ' ' + border_style;
-    styled_div += ' ' + border_color + ';';
-    styled_div += ' color: ' + textcolor + ';';
-    styled_div += ' background-color: ' + backcolor + ';"';
-    styled_div += ">your text</div>\n";
-    myValue = styled_div;
-	edInsertContent(edCanvas, myValue);
-}
-
-function wp_attnbox2_handler() {
-    
-    textcolor = "<?php echo $options['color2']; ?>";
-    backcolor = "<?php echo $options['backcolor2']; ?>";
-    border_width = "<?php echo  $options['bwidth2']; ?>";
-    border_color = "<?php echo $options['bcolor2']; ?>";
-    border_style = "<?php echo $options['bstyle2']; ?>";
-    
-    styled_div = '\n<div class="custom_attn_box" style="border: ';
-    styled_div += border_width;
-    styled_div += "px";
-    styled_div += ' ' + border_style;
-    styled_div += ' ' + border_color + ';';
-    styled_div += ' color: ' + textcolor + ';';
-    styled_div += ' background-color: ' + backcolor + ';"';
-    styled_div += ">your text</div>\n";
-    myValue = styled_div;
-	edInsertContent(edCanvas, myValue);
-}
-
-function wp_attnbox3_handler() {
-    
-    textcolor = "<?php echo $options['color3']; ?>";
-    backcolor = "<?php echo $options['backcolor3']; ?>";
-    border_width = "<?php echo  $options['bwidth3']; ?>";
-    border_color = "<?php echo $options['bcolor3']; ?>";
-    border_style = "<?php echo $options['bstyle3']; ?>";
-    
-    styled_div = '\n<div class="custom_attn_box" style="border: ';
-    styled_div += border_width;
-    styled_div += "px";
-    styled_div += ' ' + border_style;
-    styled_div += ' ' + border_color + ';';
-    styled_div += ' color: ' + textcolor + ';';
-    styled_div += ' background-color: ' + backcolor + ';"';
-    styled_div += ">your text</div>\n";
-    myValue = styled_div;
-	edInsertContent(edCanvas, myValue);
-}
-
-function wp_attnbox4_handler() {
-    
-    textcolor = "<?php echo $options['color4']; ?>";
-    backcolor = "<?php echo $options['backcolor4']; ?>";
-    border_width = "<?php echo  $options['bwidth4']; ?>";
-    border_color = "<?php echo $options['bcolor4']; ?>";
-    border_style = "<?php echo $options['bstyle4']; ?>";
-    
-    styled_div = '\n<div class="custom_attn_box" style="border: ';
-    styled_div += border_width;
-    styled_div += "px";
-    styled_div += ' ' + border_style;
-    styled_div += ' ' + border_color + ';';
-    styled_div += ' color: ' + textcolor + ';';
-    styled_div += ' background-color: ' + backcolor + ';"';
-    styled_div += ">your text</div>\n";
-    myValue = styled_div;
-	edInsertContent(edCanvas, myValue);
-}
-
-//]]></script>
-
-<?php }
-  }  // end of wp_attnbox_add_quicktag
- function edit_wpattnbox_insert_button($caption, $js_onclick, $title = '') {
-	?> if (toolbar) {
-		var theButton = document.createElement('input');
-		theButton.type = 'button';
-		theButton.value = '<?php echo $caption; ?>';
-		theButton.onclick = <?php echo $js_onclick; ?>;
-		theButton.className = 'ed_button';
-		theButton.title = "<?php echo $title; ?>";
-		theButton.id = "<?php echo "ed_{$caption}"; ?>";
-		toolbar.appendChild(theButton);
 	} 
-<?php 
-   } // end of edit_insert_button
+}
+
+
 ?>
