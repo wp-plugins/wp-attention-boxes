05d2b9c00d7e94578828a35eb1ee20f676b952f7
pythoPhpSteve <pythoPhpSteve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-19 18:26:07 +0000

    Finally added internationalization support for text and tagging to tag: 0.6.5
    git-svn-id: https://plugins.svn.wordpress.org/wp-attention-boxes/trunk@546256 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- attn-boxes-admin-menu.php
+++ attn-boxes-admin-menu.php
@@ -58,8 +58,8 @@ function attn_box_plugin_options() {
         
 	<div class="wrap">
 	<div class="icon32" id="icon-options-general"><br></div>
-	<h2>Set your Attention Box(DIV) Properties</h2>
-	<p>Configure the plugin options below</p>
+	<h2><?php _e("Set your") ?> Attention Box(DIV) <?php _e("Properties") ?></h2>
+	<p><?php _e("Configure the plugin options below") ?></p>
         
 	<div id="main_settings_section">
         
@@ -75,10 +75,10 @@ function attn_box_plugin_options() {
 	?>
           
 	<div id="first_message_div" class="custom_attn_box">
-	<span id="ft_message_title">Looks like you just activated this plugin.</span> <a id="close_first_time_message" href="#"><strong>Close</strong></a><br><br>
-	<?php echo $img_tag1; ?>You can either start creating the DIV's by simply entering your own CSS settings, <br>
-                            == or == <br>
-    <?php echo $img_tag1; ?>You can use the Auto-Populate Button at the very bottom of this page, to create a set of Starter DIV's to begin with.
+	<span id="ft_message_title"><?php _e("Looks like you just activated this plugin.")?></span> <a id="close_first_time_message" href="#"><strong><?php _e("Close")?></strong></a><br><br>
+	<?php echo $img_tag1; ?><?php _e("You can either start creating the DIV's by simply entering your own CSS settings,")?> <br>
+                            == <?php _e("or")?> == <br>
+    <?php echo $img_tag1; ?><?php _e("You can use the Auto-Populate Button at the very bottom of this page, to create a set of Starter DIV's to begin with.")?>
 							</div>
 	<?php }
 	$wp_attention_box_upgrade_to_6 = get_option("wp_attention_box_upgrade_to_6");
@@ -88,9 +88,9 @@ function attn_box_plugin_options() {
 		update_option('wp_attention_box_upgrade_to_6', 0);
 		?>
 		<div id="upgraded_first_message_div" class="custom_attn_box">
-		<span id="ft_message_title">Looks like you just upgraded to <strong>0.6.x</strong> </span> <a id="close_upgrade_message" href="#"><strong>Close</strong></a><br><br>
-		Since you have 6 empty DIV settings, you can pre-fill those with "starter styles", using the buttons at the very bottom of this Settings Page.
-		<br><br>( Note: Make sure to use the Prefill button on the Left if you wish to Keep your current DIV settings for the first 4! )
+		<span id="ft_message_title"><?php _e("Looks like you just upgraded to")?> <strong>0.6.x</strong> </span> <a id="close_upgrade_message" href="#"><strong>Close</strong></a><br><br>
+		<?php _e("Since you have 6 empty DIV settings, you can pre-fill those with 'starter styles', using the buttons at the very bottom of this Settings Page.")?>
+		<br><br><?php _e("( Note: Make sure to use the Prefill button on the Left if you wish to Keep your current DIV settings for the first 4! )")?>
 		</div>
 		
 	<?php	} 
@@ -99,39 +99,39 @@ function attn_box_plugin_options() {
         <table id="<?php echo $idx;?>" class="form-table" cellpadding="0" cellspacing="0">
             
         <tr valign="top">
-           <th> Name: </th> 
+           <th><?php _e(" Name:")?> </th> 
            <th scope="row">
               <span>  <input id="boxname_<?php echo $idx;?>" type="text" size="45" class="name_input" name="attnbox_options[box_name_<?php echo $idx;?>]" value="<?php echo $options['box_name_' . $idx]; ?>" />
               </span>
             </th>
         </tr>    
         <tr>
-            <th scope="row" >Text Color</th>
+            <th scope="row" ><?php _e("Text Color")?></th>
             <td><input id="color<?php echo $idx;?>" type="text" name="attnbox_options[color<?php echo $idx;?>]" value="<?php echo $options['color' . $idx]; ?>" />
                
            </td>
         </tr>
         <tr>
-            <th scope="row" >Background Color</th>
+            <th scope="row" ><?php _e("Background Color")?></th>
             <td><input  id="backcolor<?php echo $idx;?>" type="text" name="attnbox_options[backcolor<?php echo $idx;?>]" value="<?php echo $options['backcolor' . $idx]; ?>" />
                 
            </td>
         </tr>
         <tr>    
-            <th  scope="row">Border: </th>
+            <th  scope="row"><?php _e("Border:")?> </th>
             <td>
                  
                  <input id="bwidth<?php echo $idx;?>" size=6 type="text" name="attnbox_options[bwidth<?php echo $idx;?>]" value="<?php echo $options['bwidth' . $idx]; ?>" />
                  
                  <select id="bstyle<?php echo $idx;?>" class="border_style"  name="attnbox_options[bstyle<?php echo $idx;?>]">
-                    <option value='solid' <?php selected('solid', $options['bstyle' . $idx]); ?>>Solid</option>
-                    <option value='dotted' <?php selected('dotted', $options['bstyle' . $idx]); ?>>Dotted</option>
-                    <option value='dashed' <?php selected('dashed', $options['bstyle' . $idx]); ?>>Dashed</option>
-                    <option value='double' <?php selected('double', $options['bstyle' . $idx]); ?>>Double</option>
-                    <option value='groove' <?php selected('groove', $options['bstyle' . $idx]); ?>>Groove</option>
-                    <option value='ridge' <?php selected('ridge', $options['bstyle' . $idx]); ?>>Ridge</option>
-                    <option value='inset' <?php selected('inset', $options['bstyle' . $idx]); ?>>Inset</option>
-                    <option value='outset' <?php selected('outset', $options['bstyle' . $idx]); ?>>Outset</option>
+                    <option value='solid' <?php selected('solid', $options['bstyle' . $idx]); ?>><?php _e("Solid")?></option>
+                    <option value='dotted' <?php selected('dotted', $options['bstyle' . $idx]); ?>><?php _e("Dotted")?></option>
+                    <option value='dashed' <?php selected('dashed', $options['bstyle' . $idx]); ?>><?php _e("Dashed")?></option>
+                    <option value='double' <?php selected('double', $options['bstyle' . $idx]); ?>><?php _e("Double")?></option>
+                    <option value='groove' <?php selected('groove', $options['bstyle' . $idx]); ?>><?php _e("Groove")?></option>
+                    <option value='ridge' <?php selected('ridge', $options['bstyle' . $idx]); ?>><?php _e("Ridge");?></option>
+                    <option value='inset' <?php selected('inset', $options['bstyle' . $idx]); ?>><?php _e("Inset");?></option>
+                    <option value='outset' <?php selected('outset', $options['bstyle' . $idx]); ?>><?php _e("Outset")?></option>
                   </select>
                   
                  <input id="bcolor<?php echo $idx;?>" class="border_color" type="text" name="attnbox_options[bcolor<?php echo $idx;?>]" value="<?php echo $options['bcolor' . $idx]; ?>" /> 
@@ -140,7 +140,7 @@ function attn_box_plugin_options() {
          </tr>
          
           <tr>    
-            <th scope="row"><input id="round<?php echo $idx;?>" type="checkbox" name="attnbox_options[enable_rounded<?php echo $idx;?>]" value="1" <?php checked('1', $options['enable_rounded' . $idx]); ?>> Rounded Corners: </th>
+            <th scope="row"><input id="round<?php echo $idx;?>" type="checkbox" name="attnbox_options[enable_rounded<?php echo $idx;?>]" value="1" <?php checked('1', $options['enable_rounded' . $idx]); ?>> <?php _e("Rounded Corners:")?> </th>
             <td><input id="roundsize<?php echo $idx;?>" class="border_color" type="text" name="attnbox_options[roundsize<?php echo $idx;?>]" value="<?php echo $options['roundsize' . $idx]; ?>" /> </td> 
           </tr>  
           <tr>
@@ -148,9 +148,9 @@ function attn_box_plugin_options() {
           </tr>
           
            <tr>
-           <th>Text Alignment</th>
-            <td>Left Align: <input type="radio"  <?php checked('left', $options['align' . $idx]); ?> name="attnbox_options[align<?php echo $idx;?>]" value="left" > 
-                Center Align: <input type="radio" <?php checked('center', $options['align' . $idx]); ?> name="attnbox_options[align<?php echo $idx;?>]" value="center"></td>
+           <th><?php _e("Text Alignment")?></th>
+            <td><?php _e("Left Align:")?> <input type="radio"  <?php checked('left', $options['align' . $idx]); ?> name="attnbox_options[align<?php echo $idx;?>]" value="left" > 
+                <?php _e("Center Align:")?> <input type="radio" <?php checked('center', $options['align' . $idx]); ?> name="attnbox_options[align<?php echo $idx;?>]" value="center"></td>
           </tr>
           
          </table>
@@ -171,13 +171,13 @@ function attn_box_plugin_options() {
     <legend><b>For First Time Activation (Optional)</b></legend>
     
     <div>
-      <p>If you've just upgraded to <strong>0.6.x</strong> and you ... <br> ...<span>Don't Want to Overwrite</span> your existing first four DIV's</p>
-        <input title="You will need to Click 'Save Changes' after" class="set_div_default button-primary" id="existing_user" type="button" value="Prefill remaining 6 with Defaults">
+      <p><?php _e("If you've just upgraded to ")?><strong>0.6.x</strong> <?php _e("and you")?> ... <br> ...<span><?php _e("Don't Want to Overwrite")?></span> <?php _e("your existing first four")?> DIV's</p>
+        <input title="<?php _e('You will need to Click \'Save Changes\' after')?>" class="set_div_default button-primary" id="existing_user" type="button" value="<?php _e("Prefill remaining 6 with Defaults")?>">
     </div>
     
      <div>
-      <p>If you're new to WP Attention Boxes</p>
-        <input title="You will need to Click 'Save Changes' after" id="new_user" class="set_div_default  button-primary" type="button" value="Prefill Defaults for ALL 10 DIV Boxes">
+      <p><?php _e("If you're new to WP Attention Boxes")?></p>
+        <input title="<?php _e('You will need to Click \'Save Changes\' after')?>" id="new_user" class="set_div_default  button-primary" type="button" value='<?php _e("Prefill Defaults for ALL 10")?> DIV Boxes'>
     </div>
     
    
